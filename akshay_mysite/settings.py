7f25cafaa9af9845e1a45c3e7350f0d6192147b7
Akshay.R <akshayrnayak72@gmail.com>
2026-02-17 21:29:21 +0530

    updatedd

--- akshay_mysite/settings.py
+++ akshay_mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split(',')
+ALLOWED_HOSTS = ["blog-9742.onrender.com"]
+ 
 
 
 # Application definition
